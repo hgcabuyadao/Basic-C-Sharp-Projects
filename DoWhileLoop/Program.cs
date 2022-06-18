6c4facc19d895e40e8fab438dc8e6cc28187ce9a
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-06-17 23:49:15 -0700

    Adding Do While loop folder

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Guess a day.");
+        string day = Console.ReadLine();
+        bool isGuessed = day == "Sunday";
+
+            do
+            {
+                switch (day)
+                {
+                    case "Monday":
+                        Console.WriteLine("You guessed Monday. Try again");
+                        Console.WriteLine("Guess a day.");
+                        day = Console.ReadLine();
+                        break;
+                    case "Tuesday":
+                        Console.WriteLine("You guessed Tuesday. Try again");
+                        Console.WriteLine("Guess a day.");
+                        day = Console.ReadLine();
+                        break;
+                    case "Wednesday":
+                        Console.WriteLine("You guessed Wednesday. Try again");
+                        Console.WriteLine("Guess a day.");
+                        day = Console.ReadLine();
+                        break;
+                    case "Thursday":
+                        Console.WriteLine("You guessed Thursday. Try again");
+                        Console.WriteLine("Guess a day.");
+                        day = Console.ReadLine();
+                        break;
+                    case "Friday":
+                        Console.WriteLine("You guessed Friday. Try again");
+                        Console.WriteLine("Guess a day.");
+                        day = Console.ReadLine();
+                        break;
+                    case "Saturday":
+                        Console.WriteLine("You guessed Saturday. Try again");
+                        Console.WriteLine("Guess a day.");
+                        day = Console.ReadLine();
+                        break;
+                    case "Sunday":
+                        Console.WriteLine("You guessed it right! Sunday is the day.");
+                        isGuessed = true;
+                        break;
+                }
+            }
+            while(!isGuessed);
+            Console.ReadLine();
+        }
+    }
+}
