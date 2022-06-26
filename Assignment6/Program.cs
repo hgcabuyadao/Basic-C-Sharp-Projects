dbc3b8da1863b93480362614ff43300d84ef787e
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-06-26 03:43:52 -0700

    Adding Assignment 6

---
+++ Assignment6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Assigment 6 \n");
+        List<string> Words = new List<string>() { "Love", "Hate", "Miss", "Forget","Hate" };
+        List<string> passing = new List<string>();  
+     
+
+        foreach (string Feeling in Words)
+        {
+             
+       
+          Console.WriteLine (Feeling + "\t" + Feeling.Contains("Hate")); 
+
+            }
+            
+           Console.ReadLine();
+        }
+        }
+    
+
