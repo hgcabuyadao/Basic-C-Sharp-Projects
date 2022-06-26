c9e322e37bfa22ad83637bd9fd31596f0537226a
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-06-26 03:43:25 -0700

    Adding assignment 4 and 5

---
+++ Assignment _4_5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Assignment 4 and 5");
+      
+        
+        List<string> myFriends = new List<string> { "Lukas", "Quincy", "Harvey", "Bunch", "Lourdes", "Bunch" };
+       
+            Console.WriteLine("Enter a name:");
+            string name = Console.ReadLine();
+
+
+     
+        for (int i = 0; i < myFriends.Count; i++)
+        {
+
+            
+                if (myFriends[i] == name)
+                {
+                    Console.WriteLine("Index is at " + i);
+
+                }
+            else
+                {
+                Console.WriteLine("Name not found in the given list.");
+
+            }
+        }
+
+
+            Console.ReadLine();
+        }
+    }
+
