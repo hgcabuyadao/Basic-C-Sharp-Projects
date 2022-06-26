656279c19609001ed14f9e7cd4988d7eb476b7a4
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-06-26 03:43:02 -0700

    Adding assignment 1,2 and 3

---
+++ Assignment_1_2_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Assignment 1,2 and 3.");
+        int n = 0;
+        do
+        {
+            string[] Words = { "Love", "Hate", "Miss", "Forget" };
+            Console.WriteLine("\n");
+            Console.WriteLine("Type something.");
+            String TypeWord = Console.ReadLine();
+
+            Console.WriteLine("\n\n");
+
+            for (int i = 0; i < Words.Length; i++)
+            {
+
+                Console.WriteLine(Words[i] + TypeWord);
+            }
+            Console.WriteLine("\n\n");
+            for (int i = 0; i < Words.Length; i++)
+            {
+
+                Console.WriteLine(Words[i]);
+            }
+            
+            n++;
+        }
+        while (n < 3);
+    } 
+}
+    
+
