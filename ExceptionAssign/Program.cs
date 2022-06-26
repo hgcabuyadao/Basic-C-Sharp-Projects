a23002eafe50b448f4771fb3b499bababd2a0a09
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-06-26 12:48:14 -0700

    Adding Exception assignment folder

---
+++ ExceptionAssign/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+   
+    static void Main(string[] args)
+    {
+        try
+        {
+            List<int> Numbers = new List<int>() { 2, 4, 6, 8, 10, 12, 14 };
+        Console.WriteLine("Enter a number.");
+        int Enter = Convert.ToInt32(Console.ReadLine());
+
+        foreach (int Answer in Numbers)
+        {
+            int Divide = Answer / Enter;
+
+            Console.WriteLine("\n" + Divide);
+           
+        }
+    }  
+    catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by Zero");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number");
+        }
+        Console.ReadLine();
+
+    }
+    }
+
