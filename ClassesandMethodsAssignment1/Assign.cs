22e4563154e3389c964865af0b111f26208dcf41
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-01 01:15:31 -0700

    Add classes and method folder

---
+++ ClassesandMethodsAssignment1/Assign.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment1
+{
+    class Assign
+    {
+     public int sum()
+        {
+            int a, b=10;
+            Console.WriteLine("Enter a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            return (a + b);
+        }
+
+        public decimal Mulitply()
+        {
+            decimal c, d = 10;
+            Console.WriteLine("\nEnter a decimal: ");
+            c = Convert.ToDecimal(Console.ReadLine());
+            return decimal.Multiply(c, d);
+        }
+
+        public int Add()
+        {
+            string f = "10";
+            Console.WriteLine("\nEnter a number: ");
+            string g = Console.ReadLine();
+            int h = Convert.ToInt32(f);
+            int i = Convert.ToInt32(g);
+            return (h + i);
+        }
+
+    }
+}
---
+++ ClassesandMethodsAssignment1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethodsAssignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Assign m1 = new Assign();
+            int y = m1.sum();
+            Console.WriteLine("Sum = " + y);
+
+
+            Assign m2 = new Assign();
+            decimal x = m2.Mulitply();
+            Console.WriteLine("Product = " + x);
+
+
+            Assign m3 = new Assign();
+            int w = m3.Add();
+            Console.WriteLine("Sum = " + w);
+
+
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
