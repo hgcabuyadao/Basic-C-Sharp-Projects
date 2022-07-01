a8786088dd58224a552119fd46e78f51a22e400e
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-01 00:00:53 -0700

    Adding Method assignment folder

---
+++ MethodAssign/MathOps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign
+{
+    class MathOps
+
+    {
+        public int Sum(int p, int q)
+        {
+            return (p + q);
+
+        }
+
+        public int Multiply(int p, int q)
+        {
+            return (p * q);
+
+        }
+
+        public int Subtract(int p, int q)
+        {
+            return (p - q);
+
+        }
+    }
+}
+        
+
+
---
+++ MethodAssign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b=10,c,d,e;
+            MathOps Operation = new MathOps();
+            Console.WriteLine("Enter a number: ");
+
+            a = Convert.ToInt32(Console.ReadLine());
+            c = Operation.Sum(a,b);
+            d = Operation.Multiply(a,b);
+            e = Operation.Subtract(a,b);
+
+            Console.WriteLine("Sum = " + c);
+            Console.WriteLine("Product = " + d);
+            Console.WriteLine("Difference = " + e);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
