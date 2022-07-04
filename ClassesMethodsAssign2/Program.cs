adaa0fc1d7303cce5ccfcebde426825c9f031523
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-03 22:13:24 -0700

    Adding classes and method assign 2

---
+++ ClassesMethodsAssign2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMethodsAssign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            try
+            {
+                Addition Num = new Addition();
+
+
+                int a,b,c;
+               
+
+                Console.WriteLine("Enter first number: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter a second number: ");
+                b = Convert.ToInt32(Console.ReadLine());
+               
+
+
+                c = Num.Sum(a, b);
+                Console.WriteLine("Sum = " + c);
+
+                
+            }
+            catch(FormatException)
+            {
+              ;
+
+                Console.WriteLine("You need to enter a second number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
