c72dab2effa314074be375a52f39917c204b2f50
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-03 22:57:43 -0700

    Adding class method assign 3

---
+++ Classes MethodsAssign3/Add.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_MethodsAssign3
+{
+    class Add
+    {
+        public void Numbers()
+        {
+            int a, b, c;
+            Console.WriteLine("Enter a number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second number:");
+            b = Convert.ToInt32(Console.ReadLine());
+            c = a + 10;
+            Console.WriteLine("Sum of 10 and "+ a + " = " + c);
+            Console.WriteLine("Second number you entered is: " + b);
+
+        }
+    }
+}
---
+++ Classes MethodsAssign3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_MethodsAssign3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add Num = new Add();
+            Num.Numbers();
+            Console.ReadLine();
+        }
+    }
+}
