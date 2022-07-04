6e644f10811f42b8fe538da2d662e6691202eeb3
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-04 00:01:42 -0700

    adding method and class module 4 assign

---
+++ ClassMethodModule4/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodModule4
+{
+    class Class1
+    {
+        public void divide(int p)
+        {
+            Console.WriteLine("Quotient = " + (p / 2));
+        }
+        public void divide(out int p1)
+        {
+            Console.WriteLine("Enter a second number: ");
+            p1 = Convert.ToInt32(Console.ReadLine());
+            
+        }
+
+
+    }
+}
---
+++ ClassMethodModule4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodModule4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 p1 = new Class1();
+            Console.WriteLine("Enter a number: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            p1.divide(x);
+
+            int y;
+            p1.divide(out y);
+            Console.WriteLine("You entered number: " + y);
+            Console.ReadLine();
+        }
+        
+       
+    }
+}
