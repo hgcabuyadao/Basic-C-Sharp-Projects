8d8534161dfecb9e0d2f6a5eb8843d749373775b
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-05 01:04:56 -0700

    Adding classes and method assign 3 folder

---
+++ Classes MethodsAssign3-rev1/Add.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_MethodsAssign3
+{
+    class Add
+    {
+        public void Numbers(int p, int q)
+        {
+           
+        Console.WriteLine("Enter a number:");
+        p = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter a second number:");
+        q = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Sum of 10 + " + p + " = " + (10+p));
+        Console.WriteLine("Second number you entered is: " + q);
+
+
+        }
+    }
+}
---
+++ Classes MethodsAssign3-rev1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_MethodsAssign3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Add Num = new Add();
+            Num.Numbers(0,0);
+
+            Console.ReadLine();
+            
+         
+        }
+    }
+}
