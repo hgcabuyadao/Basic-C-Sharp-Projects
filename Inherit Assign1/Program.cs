93169e5037595d9b79ac2bd14e5bb8c042c777f6
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-06 00:27:34 -0700

    Adding Inherit Assign 1

---
+++ Inherit Assign1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inherit_Assign1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample ";
+            employee.LastName = "Student";
+            employee.SayName();
+            Console.ReadLine();
+
+
+
+
+
+
+    }
+    }
+}
