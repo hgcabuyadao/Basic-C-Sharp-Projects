9b4b3d307a1074a753e1d7528f73755802639865
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-06 22:20:56 -0700

    Adding Method Assign 2 folder

---
+++ ClassesMethodsAssign2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMethodsAssign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+                Addition Num = new Addition();
+
+
+               int a,e;
+               string b;
+
+                Console.WriteLine("Enter a number: ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter or not a second number: ");
+                b = Console.ReadLine();
+
+            if (b == "")
+            {
+             Console.WriteLine("Sum = " + Num.Sum(a));
+              }
+
+             else
+            {
+                e = Convert.ToInt32(b);
+                Console.WriteLine("Sum = " + Num.Sum(a, e));
+            }
+                
+           
+
+           
+            
+         
+            Console.ReadLine();
+        }
+           
+    }
+}
