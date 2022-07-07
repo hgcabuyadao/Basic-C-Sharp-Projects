f67f99890b0a9dff335b914a766818c385d1c536
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-06 23:24:25 -0700

    Adding abstract assign folder

---
+++ AbstractAssign1/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssign1
+{
+    class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + lastName);
+            base.SayName();
+        }
+    }
+}
