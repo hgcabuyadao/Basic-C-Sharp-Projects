86a59f174193c4585c90ac6a4abd2938e60a5efb
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-08 00:40:55 -0700

    Adding interface assignment 1 folder

---
+++ AbstractAssign1_interface/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssign1
+{
+    
+    class Employee : Person, IQuittable
+    {
+      public string ManagerName { get; set; }
+        public override void SayName()
+        {
+           
+
+            Console.WriteLine("Name: " + firstName + lastName);
+            base.SayName();
+        }
+        
+
+        public void Quit()
+        {
+            Console.WriteLine("\nIf you want to quit, call Harvey ");
+        }
+     
+        
+    }
+}
---
+++ AbstractAssign1_interface/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAssign1
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        
+
+
+        public virtual void SayName()
+        {
+
+        }
+    }
+}
