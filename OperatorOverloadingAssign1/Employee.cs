96d3bf804bb604fd834ccf4e6035e68ee0faa457
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-10 00:02:38 -0700

    Adding operator overloading folder

---
+++ OperatorOverloadingAssign1/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssign1
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+
+        public static bool operator == (Employee emp1, Employee emp2)
+        {
+            if (emp1.Id.Equals(emp2.Id))
+            {
+            return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            if (!emp1.Id.Equals(emp2.Id))
+            {
+            return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ OperatorOverloadingAssign1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingAssign1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Id = 123;
+            emp1.Name = "Harvey";
+           
+
+            Employee emp2 = new Employee();
+            emp2.Id = 123;
+            emp2.Name = "Sage";
+            
+
+            if (emp1==emp2)
+            {
+                Console.WriteLine(emp1.Name+ " and "+ emp2.Name + " have the same IDs.");
+            }
+            else
+            {
+                Console.WriteLine(emp1.Name + " and " + emp2.Name + " don't have the same IDs.");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
