bd18efa98039b3118f5a3730e24fe44f71e08251
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-12 23:26:44 -0700

    Adding struct assignment folder

---
+++ StructAssign/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number Num1 = new Number();
+            Num1.Amount = 15.12M;
+
+
+            Console.WriteLine("Value is: " + Num1.Amount);
+
+            Console.ReadLine();
+            
+
+
+
+        }
+
+
+
+    }
+    struct Number
+    {
+        public decimal Amount { get; set; }
+    }
+}
