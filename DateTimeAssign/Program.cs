3b879768218b8d0572a52b15e808ecf3ac4a219b
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-19 23:06:17 -0700

    Adding date time assignment folder

---
+++ DateTimeAssign/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now.ToString("F"));
+            Console.WriteLine("\nEnter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            DateTime After = now.AddHours(number);
+            Console.WriteLine("The time will be "+ After.ToString("t") + " after {0}",number + " hours!");
+
+
+            Console.ReadLine();
+        }
+    }
+}
