a201dfdf04b85f8191435c7379e6fad523937861
hgcabuyadao <100273070+hgcabuyadao@users.noreply.github.com>
2022-07-20 00:48:10 -0700

    Adding Additional features assign

---
+++ AdditionalFeaturesAssign/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssign
+{
+    class Program
+    {
+        public int Age;
+        public string LastName;
+
+        static void Main(string[] args)
+        {
+            const string Me = "Harvey";
+            var varInt = 1;
+            var varChar = 'h';
+            var varString = "abcdef";
+            Console.WriteLine("{0}\n{1}\n{2}", varInt, varChar, varString);
+
+            Program Myself = new Program();
+            Console.WriteLine("My name is {0} ",Me +" "+ Myself.LastName + " and I am "+ Myself.Age + " years old.");
+            Console.ReadLine();
+        }
+
+        public Program ():this(20,"Cabuyadao")
+        {
+            
+          
+        }
+
+        public Program(int Age,string LastName)
+        {
+            this.Age = Age;
+            this.LastName = LastName;
+        }
+        
+    }
+
+}
